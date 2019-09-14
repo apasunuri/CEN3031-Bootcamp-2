65eea0dc49fe1a49305c83aad6b357c58f8410de
Abinai Pasunuri <abhinay.pasunuri088@gmail.com>
2019-09-14 12:31:11 -0400

    Modified MongoDB Schema, JSON File Transofrmation, and Queries to support MongoDB Database

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,6 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,7 +21,19 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
-
+fs.readFile('listings.json', function(err, data) {
+  if(err)
+    throw err;
+  var jsonData = JSON.parse(data);
+  jsonData.entries.forEach(function(element) {
+    var schema = new Listing(element);
+    schema.save(function(err) {
+      if(err)
+        throw err;
+    });
+    console.log("Finished Adding to Database");
+  });
+});
 
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
